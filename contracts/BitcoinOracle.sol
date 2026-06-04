da4d3c3ca6820275c2341385cb5067e5b7f9aa4f
Polpe66 <f.polperio@studenti.unipi.it>
2026-06-04 15:49:41 +0200

    refactor: enhance comments for clarity in BitcoinOracle contract functions

--- contracts/BitcoinOracle.sol
+++ contracts/BitcoinOracle.sol
@@ -26,16 +26,16 @@ contract BitcoinOracle {
     // Borrower paga la fee -> oracle Python ascolta l'evento e aggiorna il saldo
     function requestUpdate(bytes32 btcAddressHash) external payable {                       // chiamata dall'esterno, msg.value è la fee che il borrower paga per richiedere l'aggiornamento, btcAddressHash è l'hash del BTC address di cui si vuole aggiornare il saldo
         require(msg.value >= MIN_ORACLE_FEE, "Fee too low");
-        emit UpdateRequested(btcAddressHash, msg.sender);                                  
+        emit UpdateRequested(btcAddressHash, msg.sender);                                   // emette un evento che segnala che è stata richiesta un'aggiornamento per un certo BTC address, l'oracle Python ascolta questo evento e quando lo riceve, legge il saldo dal blk.dat e chiama la funzione update per aggiornare il saldo
     }
 
     // Oracle Python chiama questa dopo aver letto i blk.dat
-    function update(bytes32 btcAddressHash, uint256 satoshi) external onlyOperator {
-        balances[btcAddressHash] = satoshi;
-        emit BalanceUpdated(btcAddressHash, satoshi);
+    function update(bytes32 btcAddressHash, uint256 satoshi) external onlyOperator {       // funzione che aggiorna il saldo chiamata dall'esterno solo da l'operator
+        balances[btcAddressHash] = satoshi;                                                // il mapping che da hash del BTC address restituisce il saldo in satoshi, viene aggiornato con il nuovo saldo letto dall'oracle Python
+        emit BalanceUpdated(btcAddressHash, satoshi);                                      // emette un evento che segnala che il saldo di un certo BTC address è stato aggiornato, questo evento può essere ascoltato da altri contratti o da interfacce utente per mostrare il nuovo saldo
     }
 
-    function getBalance(bytes32 btcAddressHash) external view returns (uint256) {
+    function getBalance(bytes32 btcAddressHash) external view returns (uint256) { 
         return balances[btcAddressHash];
     }
 
@@ -44,8 +44,8 @@ contract BitcoinOracle {
     }
 
     // funzione che calcola l'hash di una stringa del BTC address -> chiave bytes32
-    function hashBtcAddress(string calldata btcAddress) external pure returns (bytes32) {
-        return keccak256(abi.encodePacked(btcAddress)); //calcola solo hash
+    function hashBtcAddress(string calldata btcAddress) external pure returns (bytes32) {           // calldata perchè riceviamo una stringa dall'esterno e la salviamo nel campo calldata in modo di risparmiare gas
+        return keccak256(abi.encodePacked(btcAddress));                                             //calcola solo hash
     }
 
     // questa funzione permette a operator di ritirare le fee accumulate
