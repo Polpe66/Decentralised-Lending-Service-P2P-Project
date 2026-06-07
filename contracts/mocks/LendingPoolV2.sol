264624cfc4a280c051883d720e792c4cb077956a
Polpe66 <f.polperio@studenti.unipi.it>
2026-06-07 20:22:05 +0200

    refactor: enhance comments for clarity in LendingPoolV2 contract

--- contracts/mocks/LendingPoolV2.sol
+++ contracts/mocks/LendingPoolV2.sol
@@ -5,16 +5,16 @@ import "../LendingPool.sol";
 
 
 contract LendingPoolV2 is LendingPool {
-    uint256 public extraSlot;
+    uint256 public extraSlot;                                       // variabile extraSlot per testare l'upgradeability
     
     /// @custom:oz-upgrades-validate-as-initializer
-    function initializeV2() external reinitializer(2) {}
+    function initializeV2() external reinitializer(2) {}            
 
     function version() external pure returns (string memory) {
         return "v2";
     }
 
-    function setExtra(uint256 v) external {
+    function setExtra(uint256 v) external {                       // setta extraSlot
         extraSlot = v;
     }
 }
